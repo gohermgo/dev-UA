e232dad1dfe6ee9306f9c7e3c176f6446806fe26
gohermgo <gohnherman@gmail.com>
2023-10-07 12:32:29 +0200

    Rollback point, trying to implement the plotter class

--- src/data.py
+++ src/data.py
@@ -1,22 +1,22 @@
 import fuel_class
 import ship_class
 
-def data_list(s, t=0.0):
-    # velocity list
-    velocities = s.zero_initial_speed_vector()
-    # ( fuel * ( velocity * kg ) ) list
-    weights = [(f, [(v, s.fuel_for_trip(f, t=t, v=v), ) for v in velocities]) for f in fuel_class.fuel_list]
-    # ( fuel * ( velocity * [€ / kg] ) list
-    fuel_price = [(f, [(v, f.get_consumption_price(per_v_data)) for v, per_v_data in per_f_data]) for f, per_f_data in weights]
-    # ( fuel * ( velocity * tons ) ) ) list
-    emission_data = [(f, [(v, f.get_emission_tonnage(per_v_data)) for v, per_v_data in per_f_data]) for f, per_f_data in weights]
-    # ( fuel * ( velocity * [€ / ton] ) ) list
-    emission_price = [(f, [(v, f.get_emission_price(per_v_data)) for v, per_v_data in per_f_data]) for f, per_f_data in weights]
-    return (weights, fuel_price, emission_data, emission_price)
+# def data_list(s, t=0.0):
+#     # velocity list
+#     velocities = s.zero_initial_speed_vector()
+#     # ( fuel * ( velocity * kg ) ) list
+#     weights = [(f, [(v, s.fuel_for_trip(f, t=t, v=v), ) for v in velocities]) for f in fuel_class.fuel_list]
+#     # ( fuel * ( velocity * [€ / kg] ) list
+#     fuel_price = [(f, [(v, f.get_consumption_price(per_v_data)) for v, per_v_data in per_f_data]) for f, per_f_data in weights]
+#     # ( fuel * ( velocity * tons ) ) ) list
+#     emission_data = [(f, [(v, f.get_emission_tonnage(per_v_data)) for v, per_v_data in per_f_data]) for f, per_f_data in weights]
+#     # ( fuel * ( velocity * [€ / ton] ) ) list
+#     emission_price = [(f, [(v, f.get_emission_price(per_v_data)) for v, per_v_data in per_f_data]) for f, per_f_data in weights]
+#     return (weights, fuel_price, emission_data, emission_price)
 
-def trip_duration_list(ship, thickness=0.0):
-    # (ship * ( fuel * ( velocity * hours ) ) ) list
-    return [(v, ship.get_trip_duration(v, thickness=thickness)) for v in ship.feasible_speed_vector(thickness=thickness)]
+# def trip_duration_list(ship, thickness=0.0):
+#     # (ship * ( fuel * ( velocity * hours ) ) ) list
+#     return [(v, ship.get_trip_duration(v, thickness=thickness)) for v in ship.feasible_speed_vector(thickness=thickness)]
 
 from mpl_toolkits.basemap import Basemap
 import math
@@ -32,7 +32,7 @@ DATA_FILE = 'ice_thickness_2022.nc'
 # Open xArray dataset, find the size in meters
 dataset = xr.open_dataset(DATA_FILE)
 meters = (np.shape(dataset['sea_ice_thickness'].coords['xc'])[0] - 1) * utils.unit_distance
-
+        
 def reinit_map():
     '''
     Reinitialize map for plot points.
@@ -126,4 +126,61 @@ def save_coord_map(name):
     plt.close()
     if utils.verbose_mode:
         utils.print_separator()
+
+PARALLEL_STEP = 20.
+PARALLEL_MIN = 16.6239
+PARALLEL_MAX = 90.
+PLOTTER_PARALLELS = np.arange(PARALLEL_MIN, PARALLEL_MAX, PARALLEL_STEP)
+
+MERIDIAN_STEP = 20.
+MERIDIAN_MIN = -180.
+MERIDIAN_MAX = 180.
+PLOTTER_MERIDIANS = np.arange(MERIDIAN_MIN, MERIDIAN_MAX, MERIDIAN_STEP)
+class Plotter:
+    def __init__(self):
+        ax, fig = plt.subplots()
+        self.ax = ax
+        self.fig = fig
+
+        map = Basemap(width = meters, height = meters,
+                      projection = 'laea',
+                      lon_0 =  0., lat_0 = 90.,
+                      ellps = 'WGS84',
+                      resolution = 'i')
+
+        map.drawparallels(PLOTTER_PARALLELS)
+        map.drawmeridians(PLOTTER_MERIDIANS)
+        map.drawmapboundary(fill_color = 'gray')
+
+        cmap = mpl.cm.jet
+        cmap.set_bad('black', 1.)
+        cm = map.pcolormesh(xx, yy, ice_values, 
+                            shading='nearest', 
+                            antialiased=True, 
+                            cmap=cmap,
+                            norm = COLORMAP_NORM)
+
+        map.colorbar(cm, extend='max', label='Ice Thickness', ticks=np.arange(0.0, ICE_THICKNESS_MAX, ICE_THICKNESS_MAX / 10.0))
+
+        self.map = map
+
+    def plot_point(lon: float, lat: float, lon_west=False, c='r', m=','):
+        if lon_west:
+            x, y = self.map(360. - lon, lat)
+        else:
+            x, y = self.map(lon, lat)
+        self.map.plot(x, y, marker=m, color=c)
+
+    def save(name: str):
+        plt.title(str(name) + " map")
+        plt.show()
+        if utils.verbose_mode:
+            print("\tSaving figure...")
+        plt.savefig('images/' + str(name) + '.png')
+        if utils.verbose_mode:
+            print("\tFinished.")
+        plt.close()
+        if utils.verbose_mode:
+            utils.print_separator()
+
 utils.print_exit(__name__, __file__)
